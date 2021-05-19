bc92d9cbaa2f4f951381c91d7dce786ab58ffe96
Hoa Phan <hoa.phan-van@banvien.com.vn>
2021-05-19 14:12:14 +0700

    clean code

--- template/babel.config.js
+++ template/babel.config.js
@@ -18,9 +18,7 @@ module.exports = {
   ],
   env: {
     production: {
-      plugins: [
-        "transform-remove-console"
-      ]
-    }
-  }
+      plugins: ['transform-remove-console'],
+    },
+  },
 };
