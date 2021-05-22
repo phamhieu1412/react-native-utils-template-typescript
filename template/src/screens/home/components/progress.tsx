5b9ce3038e1a84c551ff63912f79be14c1cd23e4
hoa.phan-van <hoaphantn7604@gmail.com>
2021-05-22 20:54:04 +0700

    update progress

--- template/src/screens/home/components/progress.tsx
+++ template/src/screens/home/components/progress.tsx
@@ -7,6 +7,10 @@ import {
 } from 'react-native-utils-components';
 import { useScale } from 'react-native-utils-toolkit';
 
+const ic_step1 = require('./../../../assets/images/icons/step1.png');
+const ic_step2 = require('./../../../assets/images/icons/step2.png');
+const ic_step3 = require('./../../../assets/images/icons/step3.png');
+const ic_step4 = require('./../../../assets/images/icons/step4.png');
 const { scale } = useScale;
 
 export interface Props {
@@ -15,7 +19,9 @@ export interface Props {
 
 const SliderStepScreen: React.FC<Props> = _props => {
   const [step, setStep] = useState(1);
+  const [step2, setStep2] = useState(1);
   const [stage, setStage] = useState(2);
+  const [stage2, setStage2] = useState(2);
 
   return (
     <View style={styles.container}>
@@ -25,12 +31,31 @@ const SliderStepScreen: React.FC<Props> = _props => {
           data={[
             { text: 'Step 1', status: true },
             { text: 'Step 2', status: true },
-            { text: 'Step 3', status: false },
+            { text: 'Step 3', status: true },
             { text: 'Step 4', status: false },
           ]}
           selectIndex={step}
           onSelectIndex={index => {
             setStep(index);
+          }}
+          activeColor="#32C5FF"
+          inActiveColor="#C6CDD8"
+          selectColor="#32C5FF"
+          textColor="gray"
+          textSize={15}
+        />
+
+        <CStepProgress
+          style={{ marginTop: 20 }}
+          data={[
+            { text: 'Step 1', icon: ic_step1, status: true },
+            { text: 'Step 2', icon: ic_step2, status: true },
+            { text: 'Step 3', icon: ic_step3, status: true },
+            { text: 'Step 4', icon: ic_step4, status: false },
+          ]}
+          selectIndex={step2}
+          onSelectIndex={index => {
+            setStep2(index);
           }}
           activeColor="#32C5FF"
           inActiveColor="#C6CDD8"
@@ -47,9 +72,7 @@ const SliderStepScreen: React.FC<Props> = _props => {
             { stage: 'S1', text: 'Hello S1', status: true },
             { stage: 'S2', text: 'Hello S2', status: true },
             { stage: 'S3', text: 'Hello S3', status: true },
-            { stage: 'S4', text: 'Hello S4', status: true },
-            { stage: 'S5', text: 'Hello S5', status: true },
-            { stage: 'S6', text: 'Hello S6', status: false },
+            { stage: 'S4', text: 'Hello S4', status: false },
           ]}
           activeColor="#32C5FF"
           inActiveColor="#C6CDD8"
@@ -57,6 +80,23 @@ const SliderStepScreen: React.FC<Props> = _props => {
           selectIndex={stage}
           onSelectIndex={index => {
             setStage(index);
+          }}
+        />
+
+        <CTooltipProgress
+          style={{ marginTop: 20 }}
+          data={[
+            { icon: ic_step1, text: 'Hello S1', status: true },
+            { icon: ic_step2, text: 'Hello S2', status: true },
+            { icon: ic_step3, text: 'Hello S3', status: true },
+            { icon: ic_step4, text: 'Hello S4', status: false },
+          ]}
+          activeColor="#32C5FF"
+          inActiveColor="#C6CDD8"
+          selectColor="#32C5FF"
+          selectIndex={stage2}
+          onSelectIndex={index => {
+            setStage2(index);
           }}
         />
       </View>
